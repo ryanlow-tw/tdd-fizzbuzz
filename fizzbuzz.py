25c3ce05d5056f09dbc21cee57f11c9d77a62b22
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-09 13:07:11 +0800

    pass test if num is multiple of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,6 @@
 class FizzBuzz:
 
     def fizzbuzz(self, number):
+        if number % 3 == 0:
+            return "Fizz"
         return number
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,7 +6,7 @@ class FizzBuzzTest(unittest.TestCase):
 
     def test_should_return_same_number_if_not_multiple_of_3_and_5_if_num_is_0(self):
         result = FizzBuzz().fizzbuzz(0)
-        self.assertEqual(0, result)
+        self.assertEqual("Fizz", result)
 
     def test_should_return_same_number_if_not_multiple_of_3_and_5_if_num_is_4(self):
         result = FizzBuzz().fizzbuzz(4)
