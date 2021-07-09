914044e77f18659f6a863006f0f2f3607a32c2e8
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-09 13:07:51 +0800

    rename test for num = 0

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,7 +4,7 @@ from fizzbuzz import FizzBuzz
 
 class FizzBuzzTest(unittest.TestCase):
 
-    def test_should_return_same_number_if_not_multiple_of_3_and_5_if_num_is_0(self):
+    def test_should_return_fizz_is_0(self):
         result = FizzBuzz().fizzbuzz(0)
         self.assertEqual("Fizz", result)
 
