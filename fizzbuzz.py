0a822310af43c5daad6d80f3c2f80348862545ad
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-09 13:18:29 +0800

    pass test to raise correct error message

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ class FizzBuzz:
 
     def fizzbuzz(self, number):
         if number < 0:
-            raise ValueError
+            raise ValueError("Please input a positive integer!")
 
         if number % 3 == 0 and number % 5 == 0:
             return "FizzBuzz"
