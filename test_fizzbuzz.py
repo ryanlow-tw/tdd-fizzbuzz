7d2bcf61c887083d1988d55b51d625019f8a5774
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-09 13:05:25 +0800

    fail test should return fizz if num is multiple of 3

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -11,3 +11,7 @@ class FizzBuzzTest(unittest.TestCase):
     def test_should_return_same_number_if_not_multiple_of_3_and_5_if_num_is_4(self):
         result = FizzBuzz().fizzbuzz(4)
         self.assertEqual(4, result)
+
+    def test_should_return_fizz_if_num_is_multiple_of_3(self):
+        result = FizzBuzz().fizzbuzz(3)
+        self.assertEqual("Fizz", result)
