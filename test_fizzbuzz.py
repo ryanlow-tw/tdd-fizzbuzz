a00a440a37ad484425e7877e7349b5700c1f18bb
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-09 13:03:16 +0800

    add failing test that should return 4 if num is 4

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,6 +4,10 @@ from fizzbuzz import FizzBuzz
 
 class FizzBuzzTest(unittest.TestCase):
 
-    def test_should_return_same_number_if_not_multiple_of_3_and_5(self):
+    def test_should_return_same_number_if_not_multiple_of_3_and_5_if_num_is_0(self):
         result = FizzBuzz().fizzbuzz(0)
         self.assertEqual(0, result)
+
+    def test_should_return_same_number_if_not_multiple_of_3_and_5_if_num_is_4(self):
+        result = FizzBuzz().fizzbuzz(4)
+        self.assertEqual(4, result)
